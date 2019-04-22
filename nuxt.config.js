6e75093bbb8212262d9407c123982d5e4ccbe33e
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-04-22 15:27:51 -0700

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { 'http-equiv': "Content-Security-Policy", content: "upgrade-insecure-requests" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
